d124102881b1bd7febd46442715965a4016918dc
victorassanto <victor-159753@hotmail.com>
2025-06-10 01:09:27 -0300

    feat(backend): pagination, filter e sorting

---
+++ backend/src/common/filters/all-exceptions.filter.ts
@@ -0,0 +1,40 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+    let error = exception;
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const res = exception.getResponse();
+      if (typeof res === 'string') {
+        message = res;
+      } else if (typeof res === 'object' && res !== null) {
+        message = (res as any).message || message;
+        error = (res as any).error || error;
+      }
+    }
+
+    response.status(status).json({
+      status,
+      message,
+      error,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
